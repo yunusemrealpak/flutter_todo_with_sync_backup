30554b27519e5c15d786c3b70646e44314ffc1cc
Yunus Emre Alpak <yunusemrealpak@gmail.com>
2023-02-06 00:15:43 +0300

    added control storage

--- lib/application/home/home_cubit.dart
+++ lib/application/home/home_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_todo_with_synchronized_backup/domain/home/i_home_repository.dart';
 import 'package:flutter_todo_with_synchronized_backup/domain/model/todo_model.dart';
 import 'package:flutter_todo_with_synchronized_backup/infrastructure/cache/storage_service.dart';
 import 'package:uuid/uuid.dart';
@@ -7,8 +8,10 @@ import '../core/base_cubit.dart';
 import 'home_state.dart';
 
 class HomeCubit extends BaseCubit<HomeState> {
+  final IHomeRepository _homeRepository;
   final StorageService _storageService;
-  HomeCubit(this._storageService) : super(HomeState.initial());
+  HomeCubit(this._storageService, this._homeRepository)
+      : super(HomeState.initial());
 
   void init() {
     final model = _storageService.getData();
@@ -17,32 +20,59 @@ class HomeCubit extends BaseCubit<HomeState> {
     }
   }
 
-  void addTodo(TodoModel todo) {
+  Future<void> addTodo(TodoModel todo) async {
     todo = todo.copyWith(id: const Uuid().v4());
     final todos = state.todos;
     todos.add(todo);
+
+    safeEmit(state.copyWith(todos: todos));
 
     final model = _storageService.getData();
     var updateTodoIds = model?.updateTodoIds ?? [];
     updateTodoIds.add(todo.id!);
 
     if (model != null) {
-      _storageService.setData(
+      await _storageService.setData(
         model.copyWith(
           todos: todos,
           updateTodoIds: updateTodoIds,
         ),
       );
     } else {
-      _storageService.setData(
+      await _storageService.setData(
         StorageModel(
           todos: todos,
-          lastStorageUpdated: DateTime.now(),
           updateTodoIds: updateTodoIds,
         ),
       );
     }
-    safeEmit(state.copyWith(todos: todos));
+
+    try {
+      final result = await _homeRepository.addTodo(todo);
+      result.fold(
+        (l) => throw Exception(),
+        (r) {
+          final now = DateTime.now();
+          final model = _storageService.getData();
+          _storageService.setData(
+            model!.copyWith(
+              updateTodoIds: [],
+              lastStorageUpdated: now,
+              lastApiSynchronized: now,
+            ),
+          );
+        },
+      );
+    } catch (e) {
+      final now = DateTime.now();
+      final model = _storageService.getData();
+      _storageService.setData(
+        model!.copyWith(
+          updateTodoIds: [],
+          lastStorageUpdated: now,
+        ),
+      );
+    }
   }
 
   @override
--- lib/application/splash/splash_cubit.dart
+++ lib/application/splash/splash_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_todo_with_synchronized_backup/application/splash/splash_state.dart';
+import 'package:flutter_todo_with_synchronized_backup/application/synchronize/synchronize_cubit.dart';
 import 'package:flutter_todo_with_synchronized_backup/domain/cache/i_cache_service.dart';
 
 import '../../infrastructure/cache/storage_service.dart';
@@ -11,7 +12,28 @@ class SplashCubit extends BaseCubit<SplashState> {
   Future<void> init() async {
     setLoading(true);
     await _storageService.init();
+    _controlStorage();
     setLoading(false);
+  }
+
+  _controlStorage() async {
+    final model = _storageService.getData();
+    if (model != null) {
+      final lastStorageUpdated = model.lastStorageUpdated;
+      final lastApiSynchronized = model.lastApiSynchronized;
+      if (lastStorageUpdated != null && lastApiSynchronized != null) {
+        final difference = lastStorageUpdated.difference(lastApiSynchronized);
+        if (difference.inMinutes > 1) {
+          //context.router.replaceAll([const SynchronizeRoute()];
+        } else {
+          //context.router.replaceAll([const HomeRoute()];
+        }
+      } else if (lastStorageUpdated != null && lastApiSynchronized == null) {
+        //context.router.replaceAll([const SynchronizeRoute()];
+      } else {
+        //context.router.replaceAll([const HomeRoute()];
+      }
+    }
   }
 
   @override
